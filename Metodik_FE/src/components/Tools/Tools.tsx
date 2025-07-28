374d8766e6b028f96710f340e7bcecb5c98cb2a0
Cata Hernandez Mejia <catahm1997@gmail.com>
2025-07-27 22:02:12 -0300

    removing horizontal scroll

--- Metodik_FE/src/components/Tools/Tools.tsx
+++ Metodik_FE/src/components/Tools/Tools.tsx
@@ -9,12 +9,12 @@ const Tools = () => {
   const allToolsArray = Object.values(tools).flat();
 
   return (
-    <section className="w-screen py-16 relative bg-gray-100 text-black">
-      <div className="w-full px-6 text-center overflow-hidden">
+    <section className="w-full py-16 relative bg-gray-100 text-black">
+      <div className="w-full text-center overflow-hidden">
         <h2 className="text-4xl md:text-5xl font-extrabold mb-8 tracking-wide bg-gradient bg-clip-text text-transparent">
           {t("tools.title")}
         </h2>
-        <p className="mb-12 text-lg max-w-3xl mx-auto opacity-70 leading-relaxed">
+        <p className="text-lg max-w-3xl mx-auto opacity-70 leading-relaxed">
           {t("tools.subtitle")}
         </p>
 
